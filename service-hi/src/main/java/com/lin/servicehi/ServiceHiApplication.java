077705b3640ad022d68083e68dc2bf0e2bb2599d
linminna <linminna@52wana.com>
2018-12-25 17:37:32 +0800

    添加hystrix-dashboard

--- service-hi/src/main/java/com/lin/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/lin/servicehi/ServiceHiApplication.java
@@ -1,9 +1,12 @@
 package com.lin.servicehi;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,8 @@ import java.util.logging.Logger;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
@@ -31,8 +36,18 @@ public class ServiceHiApplication {
     }
 
     @RequestMapping("/info")
-    public String info(){
+    public String info() {
         LOG.log(Level.INFO, "calling trace service-hi ");
         return "i'm service-hi";
+    }
+
+    @RequestMapping("/hello")
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hello(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hello " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hello," + name + ",sorry,error!";
     }
 }
